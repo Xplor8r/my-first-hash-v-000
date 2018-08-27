6740db7ca29d568e7fe357e6a6a65da54567f84c
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-27 22:23:04 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,16 @@
 def my_hash
-  your_hash = {"Name" => "Mike", "Nickname" => "Mikey P", "Loves to" => "Surf, Code", "Age" => 38}
+  your_hash = {
+    "Name" => "Mike",
+    "Nickname" => "Mikey P",
+    "Loves to" => "Surf, Code",
+    "Age" => 38
+  }
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = {
+    "whale bone corsets" => 5, 2 porcelain vases and 3 oil paintings }
 end
 
 def retrieval
