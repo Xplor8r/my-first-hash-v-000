c783a8fc5a4cb69f7fc067fd1c877422a9a6659d
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-27 22:24:08 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,7 +10,10 @@ end
 
 def shipping_manifest
   the_manifest = {
-    "whale bone corsets" => 5, 2 porcelain vases and 3 oil paintings }
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
 end
 
 def retrieval
